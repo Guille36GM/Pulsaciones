d1a12031028ee87c7a414da910cfd1c63623c6d4
Guille36GM <gguette@unicesar.edu.co>
2020-09-10 22:10:56 -0500

    first commit

---
+++ Pulsaciones/js/gestion.js
@@ -0,0 +1,89 @@
+﻿function Guardar() {
+
+    var id = document.getElementById("id").value;
+    var nombre = document.getElementById("nombre").value;
+    var sexo = document.querySelector("#sexo").value;
+    var edad = document.getElementById("edad").value;
+    var pulsacion = document.getElementById("pulsacion").value;
+
+    var persona = {id: id,nombre: nombre,sexo: sexo,edad: edad,pulsacion: pulsacion};
+
+    var myJSON;
+
+    alert(myJSON = JSON.stringify(persona));
+    Limpiar();
+    localStorage.setItem(id, myJSON);    
+}
+
+function llenarTabla() {
+
+    let res = document.querySelector('#respuesta');
+    respuesta.innerHTML = '';
+
+    for (var i = 0; i <= localStorage.length - 1; i++) {
+
+        var text = localStorage.key(i);
+        var obj = JSON.parse(localStorage.getItem(text));
+
+        res.innerHTML += `
+                    <tr>
+                        <td>${obj.id}</td>
+                        <td>${obj.nombre}</td>
+                        <td>${obj.sexo}</td>
+                        <td>${obj.edad}</td>
+                        <td>${obj.pulsacion}</td>
+                    </tr>
+                `
+
+    }
+}
+
+function Consultar() {
+
+    var id = document.getElementById("id").value;
+
+    for (var i = 0; i <= localStorage.length - 1; i++) {
+
+        var text = localStorage.key(i);  
+        var obj = JSON.parse(localStorage.getItem(text));
+
+        if (id === obj.id) {
+            
+            document.getElementById("nombre").value = obj.nombre;
+            document.getElementById("sexo").value = obj.sexo;
+            document.getElementById("edad").value = obj.edad;
+            document.getElementById("pulsacion").value = obj.pulsacion;
+        }   
+    }
+}
+
+function Eliminar() {
+
+    var id = document.getElementById("id").value;
+
+    for (var i = 0; i <= localStorage.length - 1; i++) {
+
+        var text = localStorage.key(i);
+        var obj = JSON.parse(localStorage.getItem(text));
+
+        if (id === obj.id) {
+
+            localStorage.removeItem(text);
+            Limpiar();
+        }       
+    }
+}
+
+function Limpiar() {
+
+    document.getElementById("id").value = "";
+    document.getElementById("nombre").value = "";
+    document.getElementById("sexo").value = "";
+    document.getElementById("edad").value = "";
+    document.getElementById("pulsacion").value = "";
+}
+
+
+
+
+
---
+++ Pulsaciones/js/index.js
@@ -0,0 +1,41 @@
+﻿function CalcularPulsaciones() {
+
+    var edad = document.getElementById("edad").value;
+
+    var sexo = document.querySelector("#sexo").value;
+
+    var pulsaciones = 0;
+
+    if (sexo == 'Femenino') {
+        pulsaciones = (220 - edad) / 10;
+    } else if (sexo == 'Masculino') {
+        pulsaciones = (210 - edad) / 10;
+    }
+    else {
+        pulsaciones = 0;
+    }
+    document.getElementById("pulsacion").value = pulsaciones;
+}
+
+// Disable form submissions if there are invalid fields
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        // Get the forms we want to add validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === true) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                else {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    form.classList.add('was-validated');
+                }
+            }, false);
+        });
+    }, false);
+})();
